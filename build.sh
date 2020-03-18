2f9347a068735ce802bd44fbc466e4f20833de30
Sam <samzanemesis@gmail.com>
2020-03-18 11:12:29 +0200

    Change XiaomiMi6PKG to MSM8998Pkg base, cleanup directory

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p XiaomiMI6Pkg/XiaomiMI6Pkg.dsc
-gzip -c < workspace/Build/XiaomiMI6Pkg/DEBUG_GCC5/FV/XIAOMIMI6PKG_UEFI.fd >uefi.img
-cat sagit.dtb >>uefi.img
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p MSM8998Pkg/MSM8998Pkg.dsc
+gzip -c < workspace/Build/MSM8998Pkg/DEBUG_GCC5/FV/MSM8998Pkg_UEFI.fd >uefi.img
+cat dtbs/fxtec.dtb >>uefi.img
